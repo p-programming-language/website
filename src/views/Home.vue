60d2ba775a4a967c30f4df59a9ba9ed7303ffed3
R-unic <roleyspersonal@gmail.com>
2023-06-13 19:57:43 -0700

    feat: add http server example codeblock

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,15 +22,11 @@
 		</ul>
 	</div>
 
-	<div id="http-example">
-		<code class="cosmo-codeblock">
-## A very basic HTTP server<br/>
-use Server from "http"<br/>
-<br/>
-puts("Listening @ http://localhost:8080")<br/>
-Server.listen(8080, &void (string->any res, string->any req): {<br/>
-	res->send("Hello world, got %{req::path}!")<br/>
-})<br/>
-		</code>
-	</div>
+	<pre id="http-example"><code class="cosmo-codeblock"><span class="cch-com">## A very basic HTTP server</span>
+<span class="cch-kw">use</span> <span class="cch-class">Server</span> <span class="cch-kw">from</span> <span class="cch-str">"http"</span>
+
+<span class="cch-fn">puts</span><span class="cch-b1">(</span><span class="cch-str">"Listening @ <u>http://localhost:8080</u>"</span><span class="cch-b1">)</span>
+<span class="cch-class">Server</span><span class="cch-op">.</span><span class="cch-fn">listen</span><span class="cch-b1">(</span><span class="cch-num">8080</span>, <span class="cch-op">&</span><span class="cch-kw">void</span> <span class="cch-b2">(</span><span class="cch-kw">string</span><span class="cch-op">-></span><span class="cch-kw">any</span> <span class="cch-id">res</span>, <span class="cch-kw">string</span><span class="cch-op">-></span><span class="cch-kw">any</span> <span class="cch-id">req</span><span class="cch-b2">)</span><span class="cch-op">:</span> <span class="cch-b3">{</span>
+&ThickSpace;<span class="cch-id">res</span><span class="cch-op">-></span><span class="cch-fn">send</span><span class="cch-b1">(</span><span class="cch-str">"Hello world, got <span class="cch-op">%</span><span class="cch-op">{</span></span><span class="cch-id">req</span><span class="cch-op">::</span><span class="cch-id">path</span><span class="cch-str"><span class="cch-op">}</span>!"</span><span class="cch-b1">)</span>
+<span class="cch-b3">}</span><span class="cch-b1">)</span></code></pre>
 </template>
