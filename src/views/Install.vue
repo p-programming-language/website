b078fc804c8104ba0a1b5888f62da215cb356eb4
R-unic <roleyspersonal@gmail.com>
2023-10-11 00:20:18 -0400

    fix: install page

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,60 +1,38 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script lang="ts">
 export default {
-  data() {
-    return {
-      windowsInstallerURL: "",
-      osxInstallerURL: "",
-      linuxInstallerURL: ""
-    };
-  },
-  created() {
-    this.fetchLatestInstallerURL();
-  },
-  methods: {
-    async fetchLatestInstallerURL(): Promise<void> {
-      fetch("https://api.github.com/repos/cosmo-lang/cosmo-installer/releases/latest")
-        .then(response => response.json())
-        .then(data => {
-          const baseURL = `https://github.com/cosmo-lang/cosmo-installer/releases/download/${data.tag_name}/CosmoInstaller_`;
-          this.windowsInstallerURL = baseURL + "Windows.zip";
-          this.osxInstallerURL = baseURL + "OSX.zip";
-          this.linuxInstallerURL = baseURL + "Linux.zip";
-        });
-    }
-  }
+  // data() {
+  //   return {
+  //     windowsInstallerURL: "",
+  //     osxInstallerURL: "",
+  //     linuxInstallerURL: ""
+  //   };
+  // },
+  // created() {
+  //   this.fetchLatestInstallerURL();
+  // },
+  // methods: {
+  //   async fetchLatestInstallerURL(): Promise<void> {
+  //     fetch("https://api.github.com/repos/cosmo-lang/cosmo-installer/releases/latest")
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         const baseURL = `https://github.com/cosmo-lang/cosmo-installer/releases/download/${data.tag_name}/CosmoInstaller_`;
+  //         this.windowsInstallerURL = baseURL + "Windows.zip";
+  //         this.osxInstallerURL = baseURL + "OSX.zip";
+  //         this.linuxInstallerURL = baseURL + "Linux.zip";
+  //       });
+  //   }
+  // }
 };
 </script>
 
 <template>
   <h1 class="bright title push-down-much">Install</h1>
   <br/>
-  <h2 class="description-body">You can install Cosmo via the official Cosmo Installer.</h2>
-
+  <h2 class="description-body">You can install P via the source code and NPM.</h2>
   <div id="main-button-list">
 		<ul>
-      <a :href="windowsInstallerURL">
-				<button style="border-radius: 25%;" class="main-button no-border">
-          <img width="48" height="48" src="https://img.icons8.com/color/48/windows-10.png" alt="windows-10"/>
-        </button>
-			</a>
-      <a :href="osxInstallerURL">
-				<button style="border-radius: 25%;" class="main-button no-border">
-          <img width="48" height="48" src="https://img.icons8.com/color/48/000000/mac-os--v1.png" alt="mac-os--v1"/>
-        </button>
-			</a>
-      <a :href="linuxInstallerURL">
-				<button style="border-radius: 25%;" class="main-button no-border">
-          <img width="48" height="48" src="https://img.icons8.com/color/48/000000/linux--v1.png" alt="linux--v1"/>
-        </button>
-			</a>
-    </ul>
-  </div>
-
-  <h2 style="font-weight: 700;" class="description-body">OR</h2>
-  <div id="main-button-list">
-		<ul>
-      <a href="https://github.com/cosmo-lang/cosmo/archive/refs/heads/master.zip">
+      <a href="https://github.com/p-programming-language/plang/archive/refs/heads/main.zip">
 				<button style="border-radius: 25%;" class="main-button no-border">
           <img width="48" height="48" src="https://img.icons8.com/color/48/000000/git.png" alt="git"/>
         </button>
